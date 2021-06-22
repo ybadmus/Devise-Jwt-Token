3888d6b0b9c30da36ce5611d54da84fd05cf1da9
ybadmus <yusif.badmus@gmail.com>
2021-06-22 19:40:57 +0000

    add migrations for post and comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   attr_reader :current_user
+  
   skip_before_action :verify_authenticity_token
 
   before_action :set_default_format
---
+++ db/migrate/20210622193425_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_foreign_key :posts, :users
+  end
+end
