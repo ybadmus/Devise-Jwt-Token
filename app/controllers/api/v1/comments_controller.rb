fe0a5dc0bed994da6323fafbb144fd492b82e604
ybadmus <yusif.badmus@gmail.com>
2021-06-23 00:30:17 +0000

    minor update to show comment method

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,6 @@
 class API::V1::CommentsController < ApplicationController 
-  before_action :set_post
+  before_action :set_post, only: %i[index create]
+  before_action :set_comment, only: %i[show]
   before_action :authenticate_token!
 
   def index
@@ -7,10 +8,7 @@ class API::V1::CommentsController < ApplicationController
   end
 
   def show
-    comment = @post.comments.find(params[:id])
-    render json: comment, include: ['user', 'post']
-  rescue
-    render json: {errors: ["Comment could not be found"]}, status: 404 
+    render json: @comment, include: ['user', 'post']
   end
 
   def create 
@@ -30,6 +28,12 @@ class API::V1::CommentsController < ApplicationController
       @post = Post.includes(:comments).find(params[:post_id])
     rescue
       render json: {errors: ["Post could not be found"]}, status: 404 
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    rescue
+      render json: {errors: ["Comment could not be found"]}, status: 404 
     end
 
     def comment_params
